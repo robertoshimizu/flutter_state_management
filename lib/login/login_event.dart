ca3b6acd1e300154e7e29f000cc5744d2dc93ac4
robertoshimizu <rshimizubr@gmail.com>
2020-07-12 00:09:27 -0300

    test: added loginEvent tests

--- lib/login/login_event.dart
+++ lib/login/login_event.dart
@@ -3,3 +3,19 @@ part of 'login_bloc.dart';
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
 }
+
+class LoginButtonPressed extends LoginEvent {
+  final String email, password;
+
+  LoginButtonPressed({
+    @required this.email,
+    @required this.password,
+  });
+
+  @override
+  List<Object> get props => [email, password];
+
+  @override
+  String toString() =>
+      'LoginButtonPressed { email: $email, password: $password }';
+}
