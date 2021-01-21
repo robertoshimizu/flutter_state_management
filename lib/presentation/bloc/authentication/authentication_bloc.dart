50de93a3f41230f595207e34f89435a086640c0d
robertoshimizu <rshimizubr@gmail.com>
2021-01-21 15:42:44 -0300

    test: :arrow_down: need to use bloc_test 6.0.1

--- lib/presentation/bloc/authentication/authentication_bloc.dart
+++ lib/presentation/bloc/authentication/authentication_bloc.dart
@@ -12,6 +12,7 @@ part 'authentication_state.dart';
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
   final UserRepository userRepository;
+
   AuthenticationBloc({@required this.userRepository})
       : assert(userRepository != null),
         super(AuthenticationInitial());
